6ad9f85d09abf606d156f173b0068aea783a84c8
mangalam-mallah <mangalam8421@gmail.com>
2025-02-04 19:54:10 +0530

    Backend complete with some todos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cookieParser from 'cookie-parser'
 import { connectDB } from './lib/db.js';
+import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.routes.js'
 
 dotenv.config()
 const app = express()
 
+app.use(express.json());
+app.use(cookieParser())
+
 const PORT = process.env.PORT;
+
+app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
 
 app.listen(5001, () => {
     console.log("Server is running on port:", PORT);
